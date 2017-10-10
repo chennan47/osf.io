ae1354920721dc8c45bb8e4fae1af1439e9c27ae
Nan Chen <nc6r@virginia.edu>
2017-10-10 13:30:29 -0400

    Revert "[#OSF-8174]Make file view counts through OSF file storage available through the API"

--- osf/management/commands/migrate_file_view_counts_from_keen.py
+++ osf/management/commands/migrate_file_view_counts_from_keen.py
@@ -18,7 +18,7 @@ from scripts import utils as script_utils
 logger = logging.getLogger(__name__)
 
 
-def set_file_view_counts():
+def set_file_view_counts(state, *args, **kwargs):
     # get all osfstorage files which is_deleted == False, the file size in production database > 1730325
     files = OsfStorageFile.objects.all()
 
